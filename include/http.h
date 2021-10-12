68c1d730466cccceb50304f946b4d4a4ae14a6bf
ASjet <Aryan.ITFS@iCloud.com>
2021-10-13 00:10:17 +0800

    feat(http):implement class 'http_msg'

--- include/http.h
+++ include/http.h
@@ -1,15 +1,45 @@
 #ifndef HTTP_H
 #define HTTP_H
 
+#include <string>
+#include <vector>
+#include <map>
 #include "socket.h"
 
 #define CHUNK 0x4000;
-#define HEX_BUF_SIZE 16
 
-int getHTTPbody(byte * buf, size_t size);
-int getChunkSize(byte *buf, size_t size);
-int getGzip(byte * buf, size_t size);
 int decompress(byte * in, size_t in_size, byte * out, size_t out_size);
+
+struct chunk_t{
+    byte * raw_data;
+    byte * data;
+    size_t size;
+};
+
+class http_msg {
+    public:
+    http_msg() = default;
+    http_msg(byte * _HTTPMessageBuffer, size_t _BufferSize);
+    ~http_msg();
+    size_t headerSize(void);
+    size_t bodySize(void);
+    size_t chunkSize(void);
+
+    std::string type;
+    std::string ver;
+    int stat_code;
+    size_t msg_len;
+
+    std::map<std::string, std::string> headers;
+    byte * header;
+    size_t header_len;
+
+    byte * body;
+    size_t body_len;
+
+    std::vector<chunk_t> chunks;
+};
+
 
 
 #endif
--- src/http.cpp
+++ src/http.cpp
@@ -3,32 +3,17 @@
 #include <cstring>
 #include <cassert>
 #include <arpa/inet.h>
+#include <regex>
 #include "zlib.h"
 #include "http.h"
 
+using std::string;
 const int32_t spl = htonl(0x0D0A0D0A);
 const int16_t crlf = htons(0x0D0A);
 const int16_t gzip_head = htons(0x1F8B);
 
-int getHTTPbody(byte *buf, size_t size)
-{
-    byte *p = buf;
-    while (spl != *((int32_t *)p) && size--)
-        ++p;
-    p += 4;
-    return p - buf;
-}
-
-int getChunkSize(byte *buf, size_t size)
-{
-    char hex_buf[HEX_BUF_SIZE];
-    bzero(hex_buf, HEX_BUF_SIZE);
-    byte *p = buf;
-    while (crlf != *((int16_t *)p) && size--)
-        ++p;
-    strncpy(hex_buf, (char *)buf, p - buf);
-    return std::stoi(hex_buf, nullptr, 16);
-}
+const std::regex method_ptn("([A-Z]+)/([\\d|\\.]+)\\s+(\\d+)");
+const std::regex header_ptn("([\\w|-]+):\\s*(.*)");
 
 int getGzip(byte *buf, size_t size)
 {
@@ -78,4 +63,94 @@ int decompress(byte *in, size_t in_size, byte *out, size_t out_size)
         fprintf(stderr, "main: decompress: out buffer is not enough to hold output.\n");
     }
     return have;
+}
+
+http_msg::http_msg(byte *buf, size_t size)
+{
+    int cnt = size, chunk_size;
+    byte * start = nullptr, *p = buf;
+
+    while(spl != *((int32_t *)p) && cnt--)
+        ++p;
+
+    std::smatch desc, hds;
+    string header_str((char*)buf, p - buf);
+
+    if (!std::regex_search(header_str, desc, method_ptn))
+    {
+        fprintf(stderr, "http_msg: regex_search: http message not found.\n");
+        return;
+    }
+    type = desc[1];
+    ver = desc[2];
+    stat_code = std::stoi(string(desc[3]));
+
+    if (nullptr == (start = (byte*)strstr((char *)buf, desc.str(0).c_str())))
+    {
+        fprintf(stderr, "http_msg: strstr: unknown error.\n");
+        return;
+    }
+    msg_len = size - (start - buf);
+
+    headers.clear();
+    while(std::regex_search(header_str, hds, header_ptn))
+    {
+        int header_cnt = hds.size();
+        if (header_cnt = 3)
+            headers[hds.str(1)] = hds.str(2);
+        header_str = hds.suffix().str();
+    }
+
+    header = new byte[msg_len];
+    for (size_t i = 0; i < msg_len; ++i)
+        *header++ = *start++;
+
+    p = header;
+    cnt = msg_len;
+    while (spl != *((int32_t *)p) && cnt--)
+        ++p;
+    header_len = p - header;
+
+    body = p + 4;
+    body_len = msg_len - (body - header);
+
+    start = body;
+    p = body;
+    cnt = body_len;
+    while(true)
+    {
+        while(crlf != *((int16_t *)p) && cnt--)
+            ++p;
+        chunk_size = std::stoi(string((char*)start, p - start), nullptr, 16);
+        if(chunk_size == 0)
+            break;
+        p += 2;
+
+        chunk_t chunk;
+        chunk.raw_data = p;
+        chunk.size = chunk_size;
+        if(gzip_head == *((int16_t *)p))
+        {
+            byte * decmpr_buf = new byte[chunk_size];
+            bzero(decmpr_buf, chunk_size);
+            decompress(p, chunk_size, decmpr_buf, chunk_size);
+            chunk.data = decmpr_buf;
+        }
+        else
+            chunk.data = p;
+
+        chunks.push_back(chunk);
+        p += chunk_size;
+        start = p;
+    }
+}
+
+http_msg::~http_msg()
+{
+    headers.clear();
+    for(auto chunk = chunks.begin(); chunk != chunks.end(); ++chunk)
+        if(chunk->data == chunk->raw_data)
+            delete [] chunk->data;
+    delete []header;
+    chunks.clear();
 }
