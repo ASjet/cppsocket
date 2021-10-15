aa9373f41309968437678af3634724005efda50d
ASjet <Aryan.ITFS@iCloud.com>
2021-10-16 00:46:53 +0800

    feat(socketIOwin): add socket support on windows

--- include/uni_socketIO.h
+++ include/uni_socketIO.h
@@ -1,10 +1,23 @@
 #ifndef UNI_SOCKETIO_H
 #define UNI_SOCKETIO_H
 
+#include <cstdio>
 #include "Socket.h"
 
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
 //define something for Windows (32-bit and 64-bit, this part is common)
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <winsock2.h>
+
+#define ADDRESS_BUFFER_SIZE 32
+#define SOCKADDR_BUFFER_SIZE 128
+#define FD_NULL INVALID_SOCKET
+
+typedef long ssize_t;
+typedef SOCKET sockfd_t;
+
 #ifdef _WIN64
 //define something for Windows (64-bit only)
 #else
@@ -25,29 +38,31 @@
 
 #elif defined(__linux__) || defined(__unix__)
 
+#include <arpa/inet.h>
 #define SOCKET_BUFFER_SIZE 2048
-#define ADDRESS_BUFFER_SIZE 16
+#define ADDRESS_BUFFER_SIZE 32
 #define SOCKADDR_BUFFER_SIZE 128
 #define FD_NULL -1
 
 typedef int sockfd_t;
 
-void installSigHandler(void);
-bool is_open(sockfd_t sock_fd);
 
 #else
 #error "Unknown compiler"
 #endif
 
-int uni_close(sockfd_t fd);
+void installSigIntHandler(void);
 sockfd_t uni_socket(ipv_t ipv, conn_proto_t type);
-int uni_bind(sockfd_t sock_fd, port_t port, ipv_t ipv);
+bool is_open(sockfd_t sock_fd);
+void uni_close(sockfd_t fd);
+int uni_bind(sockfd_t sock_fd, port_t port, ipv_t ipv, conn_proto_t type);
 int uni_listen(sockfd_t sock_fd, int cnt);
-sockfd_t uni_accept(sockfd_t sock_fd, sock_info *peer, ipv_t ipv);
-int uni_connect(sockfd_t sock_fd, std::string host, port_t port, sock_info *peer, ipv_t ipv);
-int uni_send(sockfd_t sock_fd, const void *buf, int length);
-int uni_sendto(sockfd_t sock_fd, const void *buf, int length, std::string host, port_t port);
-int uni_recv(sockfd_t sock_fd, void *buf, int size, conn_proto_t type, sock_info *peer, ipv_t ipv);
+sockfd_t uni_accept(sockfd_t sock_fd, addr_t *peer, ipv_t ipv);
+int uni_connect(sockfd_t sock_fd, std::string host, port_t port, addr_t *peer, ipv_t ipv, conn_proto_t type);
+ssize_t uni_send(sockfd_t sock_fd, const void *buf, ssize_t length);
+ssize_t uni_sendto(sockfd_t sock_fd, const void *buf, ssize_t length, std::string host, port_t port, addr_t *peer, ipv_t ipv, conn_proto_t type);
+ssize_t uni_recv(sockfd_t sock_fd, void *buf, ssize_t size);
+ssize_t uni_recvfrom(sockfd_t sock_fd, void *buf, ssize_t size, std::string host, port_t port, addr_t *peer, ipv_t ipv, conn_proto_t type);
 bool uni_isConnecting(sockfd_t sock_fd);
 
 #endif
---
+++ src/socketIOwin.cpp
@@ -0,0 +1,331 @@
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+
+#define WIN32_LEAN_AND_MEAN
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
+#include <signal.h>
+#include "Socket.h"
+#include "uni_socketIO.h"
+#pragma comment(lib, "Ws2_32.lib")
+#pragma comment(lib, "Mswsock.lib")
+#pragma comment(lib, "AdvApi32.lib")
+////////////////////////////////////////////////////////////////////////////////
+#define AF(ipv) ((IPv4 == (ipv)) ? AF_INET : AF_INET6)
+#define PROTO(type) ((TCP == (type)) ? IPPROTO_TCP : IPPROTO_UDP)
+#define SOCK_TYPE(type) ((TCP == (type)) ? SOCK_STREAM : SOCK_DGRAM)
+////////////////////////////////////////////////////////////////////////////////
+void sigint_handler(int sig)
+{
+    for(auto s = socket_list.begin(); s != socket_list.end(); ++s)
+        s->closeSocket();
+    WSACleanup();
+    exit(sig);
+}
+int ns(const char *host,
+       const char *port,
+       struct addrinfo **result,
+       ipv_t ipv, conn_proto_t type)
+{
+    int iResult;
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF(ipv);
+    hints.ai_socktype = SOCK_TYPE(type);
+    hints.ai_protocol = PROTO(type);
+    if (nullptr == host)
+        hints.ai_flags = AI_PASSIVE;
+    if (0 != (iResult = getaddrinfo(host, port, &hints, result)))
+    {
+        fprintf(stderr,
+                "ns: getaddrinfo(%d): %s:%s",
+                iResult,
+                ((nullptr == host) ? NULL_ADDRESS : host),
+                ((nullptr == port) ? "*" : port));
+        return -1;
+    }
+    return 0;
+}
+int getPeerAddr(struct sockaddr_in *addr, addr_t *peer, ipv_t ipv)
+{
+    char addr_buf[ADDRESS_BUFFER_SIZE];
+    memset(addr_buf, 0, ADDRESS_BUFFER_SIZE);
+    if (NULL == inet_ntop(AF(ipv), &(addr->sin_addr), addr_buf, ADDRESS_BUFFER_SIZE))
+    {
+        fprintf(stderr,
+                "inet_ntop(%d)",
+                WSAGetLastError());
+        peer->addr = NULL_ADDRESS;
+        peer->port = NULL_PORT;
+        return -1;
+    }
+    peer->addr = std::string(addr_buf);
+    peer->port = ntohs(addr->sin_port);
+    return 0;
+}
+////////////////////////////////////////////////////////////////////////////////
+void installSigIntHandler(void)
+{
+    signal(SIGINT, sigint_handler);
+}
+////////////////////////////////////////////////////////////////////////////////
+sockfd_t uni_socket(ipv_t ipv, conn_proto_t type)
+{
+    int iResult;
+    WSADATA wsaData;
+
+    sockfd_t sockfd = FD_NULL;
+
+    // Initialize Winsock
+    if (0 != (iResult = WSAStartup(MAKEWORD(2, 2), &wsaData)))
+    {
+        fprintf(stderr,
+                "socketIOwin: uni_socket: WSAStartup(%d)\n",
+                iResult);
+        return FD_NULL;
+    }
+
+    // Create a SOCKET
+    if (INVALID_SOCKET == (sockfd = socket(AF(ipv), SOCK_TYPE(type), PROTO(ipv))))
+    {
+        fprintf(stderr,
+                "socketIOwin: uni_socket: socket(%d)\n",
+                WSAGetLastError());
+        return FD_NULL;
+    }
+
+    BOOL bOptVal = FALSE;
+    int bOptLen = sizeof (BOOL);
+    iResult = setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (char *) &bOptVal, bOptLen);
+    if (iResult == SOCKET_ERROR) {
+        wprintf(L"setsockopt for SO_KEEPALIVE failed with error: %u\n", WSAGetLastError());
+    }
+
+    return sockfd;
+}
+
+bool is_open(sockfd_t fd)
+{
+    return (FD_NULL != fd);
+}
+
+void uni_close(sockfd_t fd)
+{
+    if (is_open(fd))
+        closesocket(fd);
+}
+
+int uni_bind(sockfd_t sock_fd, port_t port, ipv_t ipv, conn_proto_t type)
+{
+    struct addrinfo *result = nullptr;
+    if (-1 == ns(nullptr, std::to_string(port).c_str(), &result, ipv, type))
+    {
+        fprintf(stderr, " in socketIOwin: uni_bind\n");
+        return -1;
+    }
+
+    if (SOCKET_ERROR == bind(sock_fd, result->ai_addr, (int)result->ai_addrlen))
+    {
+        fprintf(stderr,
+                "socketIOwin: uni_bind: bind(%d)\n",
+                WSAGetLastError());
+        freeaddrinfo(result);
+        return -1;
+    }
+
+    freeaddrinfo(result);
+    return 0;
+}
+
+int uni_listen(sockfd_t sock_fd, int cnt)
+{
+    if (SOCKET_ERROR == listen(sock_fd, cnt))
+    {
+        fprintf(stderr,
+                "socketIOwin: uni_listen: listen(%d)\n",
+                WSAGetLastError());
+        return -1;
+    }
+    return 0;
+}
+
+sockfd_t uni_accept(sockfd_t sock_fd, addr_t *peer, ipv_t ipv)
+{
+    sockfd_t conn_fd;
+    byte addr[SOCKADDR_BUFFER_SIZE];
+    memset(addr, 0, SOCKADDR_BUFFER_SIZE);
+    int len = SOCKADDR_BUFFER_SIZE;
+
+    if (INVALID_SOCKET == (conn_fd = accept(sock_fd, (struct sockaddr *)addr, &len)))
+    {
+        if(WSAEINTR != WSAGetLastError())
+            fprintf(stderr,
+                    "socketIOwin: uni_accept: accept(%d)\n",
+                    WSAGetLastError());
+        return FD_NULL;
+    }
+
+    if (SOCKADDR_BUFFER_SIZE == len)
+    {
+        fprintf(stderr,
+                "socketIOwin: uni_accept: buffer is too small to held entile sockaddr(byte[%d])\n",
+                SOCKADDR_BUFFER_SIZE);
+        addr[SOCKADDR_BUFFER_SIZE - 1] = '\0';
+    }
+    if (-1 == getPeerAddr((struct sockaddr_in *)addr, peer, ipv))
+        fprintf(stderr, " in socketIOwin: uni_accept\n");
+    return conn_fd;
+}
+
+int uni_connect(sockfd_t sock_fd,
+                std::string host,
+                port_t port,
+                addr_t *peer,
+                ipv_t ipv,
+                conn_proto_t type)
+{
+    struct addrinfo *result = nullptr, *p = nullptr;
+    if (0 == ns(host.c_str(), std::to_string(port).c_str(), &result, ipv, type))
+    {
+        for (p = result; nullptr != p; p = p->ai_next)
+        {
+            if (SOCKET_ERROR == connect(sock_fd, p->ai_addr, (int)p->ai_addrlen))
+                continue;
+            else
+            {
+                if (-1 == getPeerAddr((struct sockaddr_in *)p->ai_addr, peer, ipv))
+                    fprintf(stderr, " in socketIOwin: uni_connect\n");
+                freeaddrinfo(result);
+                return 0;
+            }
+        }
+        freeaddrinfo(result);
+        fprintf(stderr,
+                "socketIOwin: uni_connect: connect: unable to connect to %s:%hu\n",
+                host.c_str(),
+                port);
+    }
+    else
+        fprintf(stderr, " in socketIOwin: uni_connect\n");
+    peer->addr = NULL_ADDRESS;
+    peer->port = NULL_PORT;
+    return -1;
+}
+
+ssize_t uni_send(sockfd_t sock_fd, const void *buf, ssize_t length)
+{
+    int cnt = send(sock_fd, (const char *)buf, length, 0);
+    if (SOCKET_ERROR == cnt)
+    {
+        fprintf(stderr,
+                "socketIOwin: uni_send: send(%d): unable to send data\n",
+                WSAGetLastError());
+        return 0;
+    }
+    return cnt;
+}
+
+ssize_t uni_sendto(sockfd_t sock_fd,
+                   const void *buf,
+                   ssize_t length,
+                   std::string host,
+                   port_t port,
+                   addr_t *peer,
+                   ipv_t ipv,
+                   conn_proto_t type)
+{
+    int cnt;
+    struct addrinfo *result = nullptr, *p = nullptr;
+    if (0 == ns(host.c_str(), std::to_string(port).c_str(), &result, ipv, type))
+    {
+        for (p = result; nullptr != p; p = p->ai_next)
+        {
+            if (SOCKET_ERROR == (cnt = sendto(sock_fd, (const char *)buf, length, 0, p->ai_addr, (int)p->ai_addrlen)))
+                continue;
+            else
+            {
+                if (-1 == getPeerAddr((struct sockaddr_in *)p->ai_addr, peer, ipv))
+                    fprintf(stderr, " in socketIOwin: uni_sendto\n");
+                freeaddrinfo(result);
+                return cnt;
+            }
+        }
+        freeaddrinfo(result);
+        fprintf(stderr,
+                "socketIOwin: uni_sendto: send(%d): unable to send data to %s:%hu\n",
+                cnt,
+                host.c_str(),
+                port);
+    }
+    else
+        fprintf(stderr, " in socketIOwin: uni_sendto\n");
+    peer->addr = NULL_ADDRESS;
+    peer->port = NULL_PORT;
+    return 0;
+}
+
+ssize_t uni_recv(sockfd_t sock_fd, void *buf, ssize_t size)
+{
+    int cnt;
+    if (SOCKET_ERROR == (cnt = recv(sock_fd, (char *)buf, size, 0)))
+    {
+        fprintf(stderr,
+                "socketIOwin: uni_recv: recv(%d): unable to receive data\n",
+                cnt);
+        return 0;
+    }
+    return cnt;
+}
+
+ssize_t uni_recvfrom(sockfd_t sock_fd,
+                     void *buf,
+                     ssize_t size,
+                     std::string host,
+                     port_t port,
+                     addr_t *peer,
+                     ipv_t ipv,
+                     conn_proto_t type)
+{
+    int cnt;
+    struct addrinfo *result = nullptr, *p = nullptr;
+    if (0 == ns(host.c_str(), std::to_string(port).c_str(), &result, ipv, type))
+    {
+        memset(buf, 0, size);
+        for (p = result; nullptr != p; p = p->ai_next)
+        {
+            if (SOCKET_ERROR == (cnt = recvfrom(sock_fd, (char *)buf, size, 0, p->ai_addr, (int *)&p->ai_addrlen)))
+                continue;
+            else
+            {
+                if (-1 == getPeerAddr((struct sockaddr_in *)p->ai_addr, peer, ipv))
+                    fprintf(stderr, " in socketIOwin: uni_recvfrom\n");
+                freeaddrinfo(result);
+                return cnt;
+            }
+        }
+        freeaddrinfo(result);
+        fprintf(stderr,
+                "socketIOwin: uni_recv: recvfrom(%d): unable to receive data from %s:%hu\n",
+                cnt,
+                host.c_str(),
+                port);
+    }
+    else
+        fprintf(stderr, " in socketIOwin: uni_recvfrom\n");
+    peer->addr = NULL_ADDRESS;
+    peer->port = NULL_PORT;
+    return 0;
+}
+
+bool uni_isConnecting(sockfd_t sock_fd)
+{
+    return (FD_NULL != sock_fd);
+}
+
+#endif
