fcc6ad070bb650772fcda8f331cc673fd1b79f63
ASjet <Aryan.ITFS@iCloud.com>
2021-11-19 22:04:30 +0800

    refactor(echo): update with new cppsocket

--- example/echos.cpp
+++ example/echos.cpp
@@ -1,55 +1,56 @@
-#include <string>
+#include "Socket.h"
 #include <cstdio>
 #include <cstring>
-#include "Socket.h"
+#include <string>
+#include <system_error>
+using namespace CppSocket;
 ////////////////////////////////////////////////////////////////////////////////
 #define BUF_SIZE 2048
-char buf[BUF_SIZE];
+byte buf[BUF_SIZE];
 ////////////////////////////////////////////////////////////////////////////////
-int main(int argc, char ** argv)
-{
-    size_t cnt;
-    port_t port;
-    addr_t peer;
-    sockd_t connd;
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    printf("Usage: %s <bind_port>", argv[0]);
+    return 0;
+  }
 
-    if(argc != 2)
-    {
-        printf("Usage: %s <bind_port>", argv[0]);
-        return 0;
-    }
-    port = std::stoi(argv[1]);
+  port_t port = std::stoi(argv[1]);
+  addr_t peer;
 
-    Socket s(IPv4, TCP);
-    if(!s.avaliable())
-        return -1;
-
-    if(-1 == s.bindTo(port))
-        return -1;
-
-    if(-1 == s.listenOn(1))
-        return -1;
-
+  try {
+    Socket s(ip_v::IPv4, proto_t::TCP);
+    s.bind(port);
+    s.listen(1);
     printf("Listening on %hu\n", port);
 
-    while(true)
-    {
-        if(SOCKD_ERR == (connd = s.acceptFrom()))
-            break;
+    while (true) {
+      sock_t cs;
+      if (NULL_SOCK == (cs = s.accept()))
+        break;
 
-        peer = s.addr(connd);
-        printf("Connection with %s:%hu established\n", peer.addr.c_str(), peer.port);
+      peer = s.addr(cs);
+      printf("Connection with %s:%hu established\n", peer.ipaddr.c_str(),
+             peer.port);
 
-        while(s.isConnecting(connd))
-        {
-            if(0 == (cnt = s.recvData(buf, BUF_SIZE, connd)))
-                break;
-            printf("Receved %zd Byte(s)\n%s\n", cnt, buf);
-            s.sendData(buf, cnt, connd);
-        }
-        printf("Connection closed.\n");
+      while (s.isConnecting(cs)) {
+        memset(buf, 0, BUF_SIZE);
+        auto cnt = s.recv(buf, BUF_SIZE, cs);
+        if (0 == cnt)
+          break;
+        printf("Receved %zd byte(s) from %s:%hu\n%s\n", cnt,
+               peer.ipaddr.c_str(), peer.port, buf);
+        s.send(buf, cnt, cs);
+      }
+      printf("Connection closed.\n");
     }
-    s.closeSocket();
-    printf("Echo server shutdown.\n");
-    return 0;
+
+    s.close();
+  } catch (std::system_error &e) {
+    auto errcode = e.code().value();
+    fprintf(stderr, "error: %s(%d): %s\n", e.what(), errcode,
+            Socket::strerr(errcode));
+    return -1;
+  }
+  printf("Echo server shutdown.\n");
+  return 0;
 }
